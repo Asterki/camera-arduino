610f5bb58852999b9fe8344162a644a1d6345107
Fernando Rivera <asterki.dev@proton.me>
2024-07-29 18:48:34 -0600

    Updated Vertical Display

--- sketch_jun7a.ino
+++ sketch_jun7a.ino
@@ -10,25 +10,37 @@
 // Defining size, and output pins
 #define MAX_DEVICES 2
 #define CS_PIN 5
+const int buttonPin = 2;
 
 MD_Parola P = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+int buttonState = 0;  // variable for reading the pushbutton status
 
 void setup() {
   P.begin();
   P.setIntensity(0);
   P.displayClear();
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
   P.setTextAlignment(PA_CENTER);
   P.setFont(newFont);
   
-  P.print("1");
-  delay(2000);
+  buttonState = digitalRead(buttonPin);
 
-  P.print("2");
-  delay(2000);
+  if (buttonState == HIGH) {
+    P.print("3");
+    delay(2000);
 
-  P.print("3");
-  delay(2000);
+    P.print("2");
+    delay(2000);
+
+    P.print("1");
+    delay(2000);
+
+    P.print("!");
+    delay(2000);
+  } else {
+    P.displayClear();
+  }
 }
