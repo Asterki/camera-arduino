b2b1aa8b2bf83f95567a79be5b22c6cd697d35ea
asterki <asterki.dev@proton.me>
2024-09-22 17:40:46 -0600

    Feat: Arduino

--- hardware/index/index.ino
+++ hardware/index/index.ino
@@ -1,46 +1,44 @@
 #include <MD_Parola.h>
 #include <MD_MAX72xx.h>
 #include <SPI.h>
-#include "Parola_Fonts_data.h"
-#include <Firmata.h>
+#include "Parola_Fonts_data.h" // The custom font so that it shows sideways
 
+// Uncomment according to your hardware type
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
-#define MAX_DEVICES 2
-#define CS_PIN 5
-const int buttonPin = 13;
+//#define HARDWARE_TYPE MD_MAX72XX::GENERIC_HW
 
-MD_Parola P = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
-int buttonState = 0;
-int lastValue = LOW;
+// Defining size, and output pins
+#define MAX_DEVICES 2
+
+const int buttonPin = 2;
+#define CLK_PIN   23        // Changed for ESP32 in Wokwi
+#define DATA_PIN  19        // Changed for ESP32 in Wokwi
+#define CS_PIN    22        // Changed for ESP32 in Wokwi
+
+// Changed from hardware to software for ESP32 in Wokwi
+// HARDWARE SPI
+// MD_Parola P = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+// SOFTWARE SPI
+MD_Parola P = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+int buttonState = 0;  // variable for reading the pushbutton status
 
 void setup() {
+  Serial.begin(9600);
   P.begin();
   P.setIntensity(0);
   P.displayClear();
   pinMode(buttonPin, INPUT);
-  Firmata.setFirmwareVersion(FIRMATA_FIRMWARE_MAJOR_VERSION, FIRMATA_FIRMWARE_MINOR_VERSION);
-  Firmata.begin(57600);
 }
 
 void loop() {
-  while (Firmata.available()) {
-    Firmata.processInput();
-  }
-
-  int value = digitalRead(buttonPin);
-
-  if (value != lastValue && value == HIGH) {
-    Firmata.sendDigital(buttonPin, value);
-  }
-
-  lastValue = value;
-
+  P.setTextAlignment(PA_CENTER);
+  P.setFont(newFont);
+  
   buttonState = digitalRead(buttonPin);
 
+  // When the button is pressed, start the countdown
   if (buttonState == HIGH) {
-    P.setTextAlignment(PA_CENTER);
-    P.setFont(newFont);
-    
+    Serial.println("Button pressed");
     P.print("3");
     delay(2000);
 
